8f6e01abaff4419ddb6224c6c53ab10019a4cb73
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2022-11-03 15:20:48 +0100

    Adjust data size

--- scripts/run_tensorflow.py
+++ scripts/run_tensorflow.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     print("======================================================")
     print("Start training model")
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    model.fit(X, y, epochs=5, validation_split=0.25)
+    model.fit(X, y, epochs=5, batch_size=128, validation_split=0.25)
 
     print("Finished training model")
     print("======================================================")
