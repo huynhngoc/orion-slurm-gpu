a3465402b1a0d0b7d5f7be41b184cf05a62adfe2
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2022-11-03 15:00:34 +0100

    Updated pytorch settings

--- setups/setup_pytorch.sh
+++ setups/setup_pytorch.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+#SBATCH --ntasks=1               # 1 core(CPU)
+#SBATCH --nodes=1                # Use 1 node
+#SBATCH --job-name=singularity
+#SBATCH --mem=4G
+#SBATCH --partition=smallmem
+#SBATCH --output=outputs/singularity-%A.out
+#SBATCH --error=outputs/singularity-%A.out
+
+# If you would like to use more please adjust this.
+
+## Below you can put your scripts
+# If you want to load module
+module load singularity
+
+rm -f pytorch_gpu.sif
 singularity build --fakeroot pytorch_gpu.sif singularity/Singularity.PyTorch
