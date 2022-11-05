a8d27ffb6ace692df9820a894950e3387f5b0653
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2022-11-05 10:46:57 +0100

    Updated numba bash

--- run_numba.sh
+++ run_numba.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --ntasks=1               # 1 core(CPU)
-#SBATCH --nodes=1                # Use 1 node
-#SBATCH --job-name=gpu_tf        # sensible name for the job
+#SBATCH --ntasks=1                # 1 core(CPU)
+#SBATCH --nodes=1                 # Use 1 node
+#SBATCH --job-name=numba          # sensible name for the job
 #SBATCH --mem=16G                 # Default memory per CPU is 3GB.
 #SBATCH --partition=COURSE        # CHANGED - to use nodes containing the GPUs in the tutorial session
 #SBATCH --gres=gpu:1              # Must have - to use the GPU in the node
-#SBATCH --output=outputs/gpu-xs-%A.out
-#SBATCH --error=outputs/gpu-xs-%A.out
+#SBATCH --output=outputs/numba-%A.out
+#SBATCH --error=outputs/numba-%A.out
 
 
 # If you would like to use more please adjust this.
 
 ## Below you can put your scripts
 # If you want to load module
-module load CUDA
+module load CUDA/10.1.243-GCC-8.3.0
 module load Anaconda3
 conda activate $USER_gpu
 
